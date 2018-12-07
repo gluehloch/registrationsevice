5868370a382cfaa289439b365cc8f72683f39be8
andrewinkler <andre.winkler@web.de>
2018-12-07 14:09:21 +0100

    FIX: from schoolplanner to registration... fucking stupid...

--- src/main/java/de/awtools/registration/RegistrationController.java
+++ src/main/java/de/awtools/registration/RegistrationController.java
@@ -2,6 +2,7 @@ package de.awtools.registration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,13 @@ public class RegistrationController {
     private RegistrationService registrationService;
 
     @CrossOrigin
-    @PostMapping(value = "/register", headers = {
+    @GetMapping(path = "/ping")
+    public String ping() {
+        return "pong";
+    }
+
+    @CrossOrigin
+    @PostMapping(path = "/register", headers = {
             "Content-type=application/json" })
     public String register(RegistrationJson registration) {
 
