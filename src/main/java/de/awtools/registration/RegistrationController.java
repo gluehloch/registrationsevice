07be09dd1ec96e66bb827d15c67618b96c2ca435
andrewinkler <andre.winkler@web.de>
2019-01-05 09:50:50 +0100

    Removed .vscode directory. Updated jackson-databind to version 2.9.8

--- src/main/java/de/awtools/registration/RegistrationController.java
+++ src/main/java/de/awtools/registration/RegistrationController.java
@@ -90,45 +90,4 @@ public class RegistrationController {
         return new RegistrationValidationJson(validation);
     }
 
-    /*
-     * @CrossOrigin
-     * 
-     * @RequestMapping(value = "/season/list", method = RequestMethod.GET)
-     * public List<String> findAllSeason(HttpServletResponse response) { //
-     * return betofficeBasicJsonService.findAllSeason(); return null }
-     * 
-     * @CrossOrigin
-     * 
-     * @RequestMapping(value = "/login", method = RequestMethod.POST, headers =
-     * { "Content-type=application/json" }) public SecurityTokenJson login(
-     * 
-     * @RequestBody AuthenticationForm authenticationForm,
-     * 
-     * @RequestHeader(BetofficeHttpConsts.HTTP_HEADER_USER_AGENT) String
-     * userAgent, HttpServletRequest request) {
-     * 
-     * SecurityTokenJson securityToken = betofficeBasicJsonService.login(
-     * authenticationForm.getNickname(), authenticationForm.getPassword(),
-     * request.getSession().getId(), request.getRemoteAddr(), userAgent);
-     * 
-     * HttpSession session = request.getSession();
-     * session.setAttribute(SecurityToken.class.getName(), securityToken);
-     * 
-     * return securityToken; }
-     * 
-     * @CrossOrigin
-     * 
-     * @RequestMapping(value = "/logout", method = RequestMethod.POST, headers =
-     * { "Content-type=application/json" }) public SecurityTokenJson
-     * logout(@RequestBody LogoutFormData logoutFormData, HttpServletRequest
-     * request) {
-     * 
-     * SecurityTokenJson securityTokenJson = betofficeBasicJsonService.logout(
-     * logoutFormData.getNickname(), logoutFormData.getToken());
-     * 
-     * HttpSession session = request.getSession();
-     * session.removeAttribute(SecurityToken.class.getName());
-     * 
-     * return securityTokenJson; }
-     */
 }
