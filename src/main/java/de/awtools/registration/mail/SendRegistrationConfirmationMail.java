71582d7ec65c44d57888f06b8faf3fcacb0c5f31
Andre Winkler <andre.winkler@web.de>
2019-02-08 11:06:27 +0100

    Under construction: Send an email.

--- src/main/java/de/awtools/registration/mail/SendRegistrationConfirmationMail.java
+++ src/main/java/de/awtools/registration/mail/SendRegistrationConfirmationMail.java
@@ -1,5 +1,7 @@
 package de.awtools.registration.mail;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,9 +18,21 @@ public class SendRegistrationConfirmationMail {
 
     @Autowired
     private SendMail sendMail;
-    
-    public void send(Email recipient, Token token)  {
+
+    public void send(Email recipient, Token token) {
+        Email from = new Email("dont-reply-to-this-mail@gluehloch.de");
+        String subject = "Subject";
+        String messageText = String.format("Dies ist ein Test. token=[%s]",
+                token);
+
+        // TEST TEST TEST
         // TODO ... send an email with a link with the token ...
+        //
+        try {
+            sendMail.sendMail(from.get(), recipient.get(), subject, messageText);
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
