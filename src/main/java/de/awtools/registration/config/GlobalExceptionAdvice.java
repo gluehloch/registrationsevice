0a2018a385b4887eff0d77086c00d4be79db9b27
Andre Winkler <andre.winkler@web.de>
2019-05-02 17:19:51 +0200

    Send JSON error message on failure.

--- src/main/java/de/awtools/registration/config/GlobalExceptionAdvice.java
+++ src/main/java/de/awtools/registration/config/GlobalExceptionAdvice.java
@@ -1,17 +1,12 @@
 package de.awtools.registration.config;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import de.awtools.registration.RequestValidationException;
@@ -29,9 +24,17 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
         String bodyOfResponse = "Unknown";
         if (ex instanceof RequestValidationException) {
             RequestValidationException rvex = (RequestValidationException) ex;
-            bodyOfResponse = rvex.getValidation().getValidationCode().toString();
+            bodyOfResponse = new StringBuilder().append('{').append("\"message\": \"")
+                    .append(rvex.getValidation().getValidationCode().toString())
+                    .append("\"").append('}').toString();
+
+            // Json.createObjectBuilder()
+            // .add("message",
+            // rvex.getValidation().getValidationCode().toString())
+            // .build().toString();
+
         }
-        
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 
@@ -42,25 +45,20 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
     // @ExceptionHandler(value = /*ResponseStatusException*/ Exception.class)
     // @ResponseStatus(HttpStatus.BAD_REQUEST)
     /*
-    public ModelAndView defaultErrorHandler(
-            HttpServletRequest req, Exception e) throws Exception {
-
-        // If the exception is annotated with @ResponseStatus rethrow it and let
-        // the framework handle it - like the OrderNotFoundException example
-        // at the start of this post.
-        // AnnotationUtils is a Spring Framework utility class.
-        if (AnnotationUtils.findAnnotation(e.getClass(),
-                ResponseStatus.class) != null)
-            throw e;
-
-        // Otherwise setup and send the user to a default error-view.
-        ModelAndView mav = new ModelAndView();
-        mav.setStatus(HttpStatus.BAD_REQUEST);
-        mav.addObject("exception", e);
-        mav.addObject("url", req.getRequestURL());
-        mav.setViewName(DEFAULT_ERROR_VIEW);
-        return mav;
-    }
-    */
+     * public ModelAndView defaultErrorHandler( HttpServletRequest req,
+     * Exception e) throws Exception {
+     * 
+     * // If the exception is annotated with @ResponseStatus rethrow it and let
+     * // the framework handle it - like the OrderNotFoundException example //
+     * at the start of this post. // AnnotationUtils is a Spring Framework
+     * utility class. if (AnnotationUtils.findAnnotation(e.getClass(),
+     * ResponseStatus.class) != null) throw e;
+     * 
+     * // Otherwise setup and send the user to a default error-view.
+     * ModelAndView mav = new ModelAndView();
+     * mav.setStatus(HttpStatus.BAD_REQUEST); mav.addObject("exception", e);
+     * mav.addObject("url", req.getRequestURL());
+     * mav.setViewName(DEFAULT_ERROR_VIEW); return mav; }
+     */
 
 }
