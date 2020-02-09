85f5e22fe8420ea628f37c2ab849f61ccdb4a324
andrewinkler <andre.winkler@web.de>
2020-02-09 18:15:09 +0100

    JWT generator test.

--- src/test/java/de/awtools/registration/jwt/JasonWebTokenTest.java
+++ src/test/java/de/awtools/registration/jwt/JasonWebTokenTest.java
@@ -1,27 +1,93 @@
 package de.awtools.registration.jwt;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.security.Key;
-
-import org.junit.jupiter.api.Test;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Test JWT.
- * 
+ *
  * @author Andre Winkler
  */
 public class JasonWebTokenTest {
 
-	@Test
-	public void jsonWebToken() {
-		Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-		String jws = Jwts.builder().setSubject("Joe").signWith(key).compact();
-		assertThat(Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody().getSubject()).isEqualTo("Joe");
-	}
+    private Date yesterday;
+    private Date tomorrow;
+    private Key key;
+
+    @BeforeEach
+    public void before() {
+        yesterday = Date.from(LocalDate.now().minusDays(1).atStartOfDay()
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
+        tomorrow = Date.from(LocalDate.now().plusDays(1).atStartOfDay()
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
+
+        key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
+
+    @Test
+    public void generatePublicPrivateKeyPair() {
+        // Erstellt einen asymmetrischen (public/private) Schluessel.
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+
+        assertThat(privateKey).isNotNull();
+        assertThat(publicKey).isNotNull();
+    }
+
+    @Test
+    public void jsonWebToken() {
+        // Mit diesem Befehl kann man den Schluessel in einen String umwandeln und abspeichern.
+        // Dabei nicht vergessen: Das ist der geheime Schluessel.
+        String secretString = Encoders.BASE64.encode(key.getEncoded());
+        assertThat(secretString).isNotBlank();
+
+        String jws = Jwts.builder()
+                .setHeaderParam("betoffice", "1.0")
+                .setSubject("Joe")
+                .setExpiration(tomorrow)
+                .signWith(key)
+                .compact();
+
+        assertThat(Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(jws)
+                .getBody()
+                .getSubject()).isEqualTo("Joe");
+    }
+
+    @Test
+    public void expiredJwt() {
+        String jws = Jwts.builder()
+                .setHeaderParam("betoffice", "1.0")
+                .setSubject("Joe")
+                .setExpiration(yesterday)
+                .signWith(key)
+                .compact();
+
+        assertThatThrownBy(() -> Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(jws)
+                .getBody()
+                .getSubject())
+                .isInstanceOf(io.jsonwebtoken.ExpiredJwtException.class);
+    }
 
 }
