4a49a62f3b6f4a373b1839f71b2e1d1410d077d6
Andre Winkler <gluehloch@googlemail.com>
2021-01-03 16:21:30 +0100

    WIP: Added an example for GreenMail usage.

--- src/test/java/de/awtools/registration/mail/MailControllerTest.java
+++ src/test/java/de/awtools/registration/mail/MailControllerTest.java
@@ -1,13 +1,29 @@
 package de.awtools.registration.mail;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.mail.internet.MimeMessage;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
 
 public class MailControllerTest {
 
     @RegisterExtension
     static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP);
+
+    @Test
+    @DisplayName("Send test")
+    void testSend() {
+        GreenMailUtil.sendTextEmailTest("to@localhost", "from@localhost", "some subject", "some body");
+        final MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+        final MimeMessage receivedMessage = receivedMessages[0];
+        assertThat(GreenMailUtil.getBody(receivedMessage)).isEqualTo("some body");
+    }
 
 }
