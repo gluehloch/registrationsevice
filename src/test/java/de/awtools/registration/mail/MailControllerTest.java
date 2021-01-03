0115814ee70c2dfa3b824778eb75791c63e8364e
Andre Winkler <gluehloch@googlemail.com>
2021-01-03 16:15:10 +0100

    FIX: GreenMail Junit5 extension.

--- src/test/java/de/awtools/registration/mail/MailControllerTest.java
+++ src/test/java/de/awtools/registration/mail/MailControllerTest.java
@@ -2,6 +2,9 @@ package de.awtools.registration.mail;
 
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
 public class MailControllerTest {
 
     @RegisterExtension
