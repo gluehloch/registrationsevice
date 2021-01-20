434ab6346b92f521a60a1b4a4faa68e51584cc61
Andre Winkler <gluehloch@googlemail.com>
2021-01-20 07:12:34 +0100

    WIP: User/Role/Privilege relation test.

--- src/test/java/de/awtools/registration/user/BetofficeApplicationUserRoleTest.java
+++ src/test/java/de/awtools/registration/user/BetofficeApplicationUserRoleTest.java
@@ -2,6 +2,7 @@ package de.awtools.registration.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.transaction.Transactional;
@@ -31,41 +32,49 @@ import de.awtools.registration.user.RoleEntity.RoleBuilder;
 @Transactional
 @Rollback
 public class BetofficeApplicationUserRoleTest {
-    
+
     @Autowired
     private ApplicationRepository applicationRepository;
-    
+
     @BeforeAll
     public void before() {
         ApplicationEntity betofficeApplication = ApplicationBuilder.of("betoffice").description("Betoffice Community");
 
-       PrivilegeEntity createSeason = PrivilegeBuilder.of("CREATE season");
-       PrivilegeEntity updateSeason = PrivilegeBuilder.of("UPDATE season");
-       PrivilegeEntity deleteSeason = PrivilegeBuilder.of("DELETE season");
-       PrivilegeEntity readSeason = PrivilegeBuilder.of("READ season");
-       
-       PrivilegeEntity createRound = PrivilegeBuilder.of("CREATE round");
-       PrivilegeEntity updateRound = PrivilegeBuilder.of("UPDATE round");
-       PrivilegeEntity deleteRound = PrivilegeBuilder.of("DELETE round");
-       PrivilegeEntity readRound = PrivilegeBuilder.of("READ round");
-       
-       PrivilegeEntity createGame = PrivilegeBuilder.of("CREATE game");
-       PrivilegeEntity updateGame = PrivilegeBuilder.of("UPDATE game");
-       PrivilegeEntity deleteGame = PrivilegeBuilder.of("DELETE game");
-       PrivilegeEntity readGame = PrivilegeBuilder.of("READ game");
-       
-       PrivilegeEntity createTipp = PrivilegeBuilder.of("CREATE tipp");
-       PrivilegeEntity updateTipp = PrivilegeBuilder.of("UPDATE tipp");
-       PrivilegeEntity deleteTipp = PrivilegeBuilder.of("DELETE tipp");
-       PrivilegeEntity readTipp = PrivilegeBuilder.of("READ tipp");
-       
-       RoleEntity admin = RoleBuilder.of("ADMIN");
-       RoleEntity communityManager = RoleBuilder.of("Community Manager");
-       RoleEntity tipper = RoleBuilder.of("Tipper");
-        
+        PrivilegeEntity createSeason = PrivilegeBuilder.of("CREATE season");
+        PrivilegeEntity updateSeason = PrivilegeBuilder.of("UPDATE season");
+        PrivilegeEntity deleteSeason = PrivilegeBuilder.of("DELETE season");
+        PrivilegeEntity readSeason = PrivilegeBuilder.of("READ season");
+
+        PrivilegeEntity createRound = PrivilegeBuilder.of("CREATE round");
+        PrivilegeEntity updateRound = PrivilegeBuilder.of("UPDATE round");
+        PrivilegeEntity deleteRound = PrivilegeBuilder.of("DELETE round");
+        PrivilegeEntity readRound = PrivilegeBuilder.of("READ round");
+
+        PrivilegeEntity createGame = PrivilegeBuilder.of("CREATE game");
+        PrivilegeEntity updateGame = PrivilegeBuilder.of("UPDATE game");
+        PrivilegeEntity deleteGame = PrivilegeBuilder.of("DELETE game");
+        PrivilegeEntity readGame = PrivilegeBuilder.of("READ game");
+
+        PrivilegeEntity createTipp = PrivilegeBuilder.of("CREATE tipp");
+        PrivilegeEntity updateTipp = PrivilegeBuilder.of("UPDATE tipp");
+        PrivilegeEntity deleteTipp = PrivilegeBuilder.of("DELETE tipp");
+        PrivilegeEntity readTipp = PrivilegeBuilder.of("READ tipp");
+
+        List<PrivilegeEntity> createUpdateDeleteReadSeason =
+            List.of(createSeason, updateSeason, deleteSeason,
+                createRound, updateRound, deleteRound, readRound,
+                createGame, updateGame, deleteGame, readGame);
+
+        List<PrivilegeEntity> createUpdateDeleteReadTipp =
+            List.of(createTipp, updateTipp, deleteTipp, readTipp);
+
+        RoleEntity admin = RoleBuilder.of("ADMIN");
+        RoleEntity communityManager = RoleBuilder.of("Community Manager");
+        RoleEntity tipper = RoleBuilder.of("Tipper");
+
         applicationRepository.save(betofficeApplication);
     }
-    
+
     @DisplayName("Repository test: Find all users")
     @Test
     @Tag(Tags.REPOSITORY)
@@ -73,7 +82,7 @@ public class BetofficeApplicationUserRoleTest {
         PrivilegeEntity readPriv = PrivilegeEntity.PrivilegeBuilder.of("READ");
         PrivilegeEntity writePriv = PrivilegeEntity.PrivilegeBuilder.of("WRITE");
         PrivilegeEntity deletePriv = PrivilegeEntity.PrivilegeBuilder.of("DELETE");
-        
+
         privilegeRepository.saveAll(Set.of(readPriv, writePriv, deletePriv));
 
         Iterable<PrivilegeEntity> privileges = privilegeRepository.findAll();
@@ -81,5 +90,5 @@ public class BetofficeApplicationUserRoleTest {
         assertThat(privileges).hasSize(3);
         assertThat(privileges).contains(readPriv, writePriv, deletePriv);
     }
-    
+
 }
