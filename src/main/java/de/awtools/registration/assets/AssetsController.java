8782457d485015f3ec7eb7d55085b06a0c4fbb6f
Andre Winkler <gluehloch@gmail.com>
2023-04-15 18:44:20 +0200

    It does not work...!

---
+++ src/main/java/de/awtools/registration/assets/AssetsController.java
@@ -0,0 +1,53 @@
+package de.awtools.registration.assets;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.awtools.registration.HttpConst;
+import de.awtools.registration.time.DateTimeJson;
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/assets")
+public class AssetsController {
+
+    /**
+     * Ping the cookie service.
+     *
+     * @return Web-Service reachable?
+     */
+    @ApiOperation(value = "ping", nickname = "ping", response = DateTimeJson.class, notes = "Ping this service. Is it reachable?")
+    @CrossOrigin
+    @GetMapping(path = "/ping", produces = HttpConst.JSON_UTF_8)
+    public DateTimeJson ping() {
+        DateTimeJson dateTimeJson = new DateTimeJson();
+        return dateTimeJson.setDateTime(LocalDateTime.now());
+    }
+
+    /*
+    @CrossOrigin
+	@GetMapping(name = "/x/{resource}", produces = HttpConst.JSON_UTF_8)
+	public HttpEntity<String> resource(@PathVariable String resource) throws IOException {
+		InputStream resourceAsStream = AssetsController.class.getResourceAsStream(resource);
+		return new HttpEntity<String>(new String(resourceAsStream.readAllBytes(), StandardCharsets.UTF_8));
+	}
+	*/
+
+    @CrossOrigin
+	@GetMapping(name = "/x/{resource}", produces = HttpConst.JSON_UTF_8)
+	public String resource(@PathVariable String resource) throws IOException {
+		InputStream resourceAsStream = AssetsController.class.getResourceAsStream(resource);
+		return new String(resourceAsStream.readAllBytes(), StandardCharsets.UTF_8);
+	}
+
+}
